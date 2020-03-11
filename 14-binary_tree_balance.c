144e7041646d629db1930828f76277cd5d4d9a88
Gaspela04 <samirmillanorozco@hotmail.com>
2020-03-11 19:25:44 +0000

    Balance and height

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -21,3 +21,23 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	return (avl_left - avl_right);
 }
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+
+	size_t height_left = 0;
+	size_t height_right = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		height_left = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		height_right = 1 + binary_tree_height(tree->right);
+
+	if (height_left > height_right)
+		return (height_left);
+
+	return (height_right);
+}
